96ec0ff7e8c97b7bcc620f3bdb7eb2d440f9611b
Kiwi_Lin <opda0887@gmail.com>
2022-10-11 09:35:37 +0800

    add crawler

--- app.py
+++ app.py
@@ -16,6 +16,32 @@ handler = WebhookHandler('616c083acfb57650e6cc0c5d20b351aa')
 
 line_bot_api.push_message('U6ea49ab8c64a47cfa18fd9993d1ac351', TextSendMessage(text='Can Start~'))
 
+# crawler
+from urllib import request as req
+from fake_useragent import UserAgent
+import bs4
+
+def IBaha():
+    url = "https://forum.gamer.com.tw/B.php?bsn=27487"
+    #建立 Request 物件，附加Headers的資訊，讓google伺服器以為我們是一般使用者
+    ua = UserAgent().edge
+    request=req.Request(url, headers={
+        "User-Agent": ua
+    })
+    with req.urlopen(request) as response:
+        data=response.read().decode("utf-8")
+    #解析原始碼，得到每篇標題
+    root=bs4.BeautifulSoup(data, "html.parser")
+    titles=root.find_all("div", class_="b-list__tile") #找全部 class="title"的div標籤  #把div裡面有內容的影印出來
+    content = ""
+    for title in titles:
+        if (title.p != None):
+            print(title.p.text)
+            content += f"{title.p.text}\n"
+    content += f"{url}"
+    return content
+
+
 # 監聽所有來自 /callback 的 Post Request 
 @app.route("/callback", methods=['POST']) 
 def callback():     
@@ -34,9 +60,10 @@ def callback():
 # send text
 @handler.add(MessageEvent, message=TextMessage) 
 def handle_message(event):
+    a = IBaha()
     message = event.message.text
     if re.match("搜尋", message):
-        line_bot_api.reply_message(event.reply_token,TextSendMessage("查詢中"))
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(a))
     else:
         line_bot_api.reply_message(event.reply_token,TextSendMessage(message))
 
