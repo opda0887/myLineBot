3b8884679ea4aa5484111789ddcadf08e47c5ea6
Kiwi_Lin <opda0887@gmail.com>
2022-10-11 08:52:16 +0800

    first commit

---
+++ Procfile
@@ -0,0 +1,16 @@
+web: python app.py
+flask
+numpy
+beautifulsoup4 
+requests 
+scipy 
+pandas 
+lxml
+line-bot-sdk
+datetime  
+matplotlib 
+gspread
+oauth2client 
+pyimgur 
+mpl_finance 
+arrow
---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, request, abort
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import *
+
+app = Flask(__name__)  
+# Channel Access Token 
+line_bot_api = LineBotApi('qnz4kuyCErzakxej9E1OTYrKrE2PKEGouSgMhSncmWrrz4iarnpd3DrRkkN/YUtMFCmiogxiEQifR2NMKWTi8Oy6tgyKJgf2/L6jPrV4o14kl9zDuBxNOZnBbguF636Sb9i5J81tEQF2hXZuMwQP4AdB04t89/1O/w1cDnyilFU=')  
+# Channel Secret
+handler = WebhookHandler('616c083acfb57650e6cc0c5d20b351aa')
+
+line_bot_api.push_message('U6ea49ab8c64a47cfa18fd9993d1ac351', TextSendMessage(text='Can Start~'))
+
+# 監聽所有來自 /callback 的 Post Request 
+@app.route("/callback", methods=['POST']) 
+def callback():     
+    # get X-Line-Signature header value     
+    signature = request.headers['X-Line-Signature']
+    # get request body as text     
+    body = request.get_data(as_text=True)     
+    app.logger.info("Request body: " + body)      
+    # handle webhook body     
+    try:         
+        handler.handle(body, signature)     
+    except InvalidSignatureError:         
+        abort(400)      
+    return 'OK'
+
+# send text
+@handler.add(MessageEvent, message=TextMessage) 
+def handle_message(event):     
+    message = TextSendMessage(text=event.message.text+" test")     
+    line_bot_api.reply_message(event.reply_token,message)
+
+# main
+#主程式 
+import os 
+if __name__ == "__main__":    
+    port = int(os.environ.get('PORT', 5000))     
+    app.run(host='0.0.0.0', port=port)
