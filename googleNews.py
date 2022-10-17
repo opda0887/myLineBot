c299a749e23e8da8c3cbdc5eedb98273416612be
Kiwi_Lin <opda0887@gmail.com>
2022-10-17 18:27:42 +0800

    upd: news setting

--- googleNews.py
+++ googleNews.py
@@ -3,6 +3,35 @@ from bs4 import BeautifulSoup
 
 
 def GoogleNews_crawler():
+    url = "https://news.ltn.com.tw/list/breakingnews/popular"
+    response = requests.get(url)
+
+    content = ""
+
+    # 解析網頁原始碼
+    soup = BeautifulSoup(response.text, "html.parser")
+    # 找到標題資料
+    data = soup.find_all("a", {"class": "tit"})
+    
+    for index, d in enumerate(data):
+        if index <5:
+            if (d != None):
+                title = d.get("title")
+                href = d.get("href")
+                content += "{}\n{}\n\n".format(title, href)
+        else:
+            break
+    return content
+
+# print(GoogleNews_crawler())
+
+
+"""
+import requests
+from bs4 import BeautifulSoup
+
+
+def GoogleNews_crawler():
     url = "https://news.google.com/topstories?hl=zh-TW&gl=TW&ceid=TW:zh-Hant"
     base = "https://news.google.com/articles/"
     response = requests.get(url)
@@ -26,3 +55,5 @@ def GoogleNews_crawler():
     return content
 
 # print(GoogleNews_crawler())
+
+"""
